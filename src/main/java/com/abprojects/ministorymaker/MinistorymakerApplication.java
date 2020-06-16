b716e25529245e525df7e4f4f917462ce2b4fc0c
ariabee <abrown72@uvm.edu>
2020-06-16 10:53:23 -0400

    initialized Spring Boot project

---
+++ src/main/java/com/abprojects/ministorymaker/MinistorymakerApplication.java
@@ -0,0 +1,13 @@
+package com.abprojects.ministorymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistorymakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistorymakerApplication.class, args);
+	}
+
+}
